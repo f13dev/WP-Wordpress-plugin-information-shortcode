d4ae5861ba07dc88eaf3afbfc78c2650cd469593
James Valentine <jv@f13dev.com>
2016-07-05 20:47:59 +0100

    Start of styled template

---
+++ wp-api-formatted.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>WordPress plugins API template</title>
+        <link rel="stylesheet" type="text/css" href="css/wp-api.css">
+    </head>
+    <body>
+        <div class="wp-container">
+            
+        </div>
+    </body>
+</html>
