772d104ab0c4abe2a70e25ce7ef956f1e32dbd1f
James Valentine <jv@f13dev.com>
2016-07-06 12:41:52 +0100

    Registered base shortcode

--- wp-wordpress-plugin-information-shortcode.php
+++ wp-wordpress-plugin-information-shortcode.php
@@ -24,3 +24,10 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software 
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
+
+add_shortcode( 'wpplugin', 'wp_plugin_information');
+
+function wp_plugin_information()
+{
+    include_once('wp-api/wp-api-formatted.php');
+}
