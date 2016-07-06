3c88a859c75437947188e58ab5760255d8208d1c
James Valentine <jv@f13dev.com>
2016-07-06 12:33:52 +0100

    Adding copyright information

--- wp-wordpress-plugin-information-shortcode.php
+++ wp-wordpress-plugin-information-shortcode.php
@@ -9,3 +9,18 @@ Author URI: http://f13dev.com
 Text Domain: wp-plugin-information-shortcode
 License: GPLv3
 */
+
+/*
+Copyright 2016 James Valentine - f13dev (jv@f13dev.com)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software 
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
