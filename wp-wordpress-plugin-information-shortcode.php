04592f0e1683e3659fe57ee88e1527f9905743ae
James Valentine <jv@f13dev.com>
2016-07-06 12:28:34 +0100

    Added plugin WP headder

--- wp-wordpress-plugin-information-shortcode.php
+++ wp-wordpress-plugin-information-shortcode.php
@@ -1,1 +1,11 @@
 <?php
+/*
+Plugin Name: WordPress plugin information shortcode
+Plugin URI: http://f13dev.com
+Description: This plugin enables you to enter shortcode on any page or post in your blog to show information about a WP plugin you have written.
+Version: 1.0
+Author: James Valentine - f13dev
+Author URI: http://f13dev.com
+Text Domain: wp-plugin-information-shortcode
+License: GPLv3
+*/
