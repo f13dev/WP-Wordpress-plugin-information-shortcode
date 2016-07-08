fb0b02c0283b06e9d2b87b58227f3dabb177bbf3
James Valentine <jv@f13dev.com>
2016-07-08 10:10:21 +0100

    Updating name

--- wp-wordpress-plugin-information-shortcode.php
+++ wp-wordpress-plugin-information-shortcode.php
@@ -4,7 +4,7 @@ Plugin Name: WordPress plugin information shortcode
 Plugin URI: http://f13dev.com
 Description: This plugin enables you to enter shortcode on any page or post in your blog to show information about a WP plugin you have written.
 Version: 1.0
-Author: James Valentine - f13dev
+Author: Jim Valentine - f13dev
 Author URI: http://f13dev.com
 Text Domain: wp-plugin-information-shortcode
 License: GPLv3
